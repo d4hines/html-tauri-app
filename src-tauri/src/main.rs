bcaaf694d372f49f80e4b86453a516094b1be77c
Daniel Hines <d4hines@gmail.com>
2024-03-21 11:41:52 -0400

    decide page dynamically

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -10,6 +10,14 @@ fn greet(name: &str) -> String {
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            // I can decide at runtime whether to launch index.html or foo.html
+            let url = if true { "foo.html" } else { "index.html" };
+            let _main_window =
+                tauri::WindowBuilder::new(app, "main", tauri::WindowUrl::App(url.into()))
+                    .build()?;
+            Ok(())
+        })
         .run(tauri::generate_context!("tauri.conf.foo.json"))
         .expect("error while running tauri application");
 }
